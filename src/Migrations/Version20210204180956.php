7fb34ae5380bdc9d2fcadaf53f4218c1b1734e0a
Piotr Kardasz <piotr.kardasz91@gmail.com>
2021-02-04 20:27:21 +0100

    Update migration remove redundant comments

--- src/Migrations/Version20210204180956.php
+++ src/Migrations/Version20210204180956.php
@@ -7,9 +7,6 @@ namespace App\Migrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20210204180956 extends AbstractMigration
 {
     public function getDescription() : string
@@ -19,13 +16,11 @@ final class Version20210204180956 extends AbstractMigration
 
     public function up(Schema $schema) : void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE sylius_product ADD color VARCHAR(255) DEFAULT NULL');
     }
 
     public function down(Schema $schema) : void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE sylius_product DROP color');
     }
 }
